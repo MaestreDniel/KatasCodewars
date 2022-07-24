4a83205038167e82e2836ba66a8060ba2f6e6b04
Daniel Maestre <dmaestre@cifpfbmoll.eu>
2022-07-24 12:47:15 +0200

    Solución 2 para predictAge.js

--- javascript/predictAge.js
+++ javascript/predictAge.js
@@ -1,5 +1,7 @@
 // https://www.codewars.com/kata/5aff237c578a14752d0035ae
 
+// Solución 1
+
 function predictAge(a1, a2, a3, a4, a5, a6, a7, a8) {
   let ages = [a1, a2, a3, a4, a5, a6, a7, a8];
 
@@ -14,4 +16,18 @@ function predictAge(a1, a2, a3, a4, a5, a6, a7, a8) {
   return result;
 }
 
-console.log(predictAge(64, 73, 56, 82, 62, 77, 70, 62));
+console.log(predictAgeTwo(64, 73, 56, 82, 62, 77, 70, 62));
+
+// Solución 2
+
+function predictAgeTwo(...age) {
+  let ages = [...age];
+
+  ages.forEach((age, i) => {
+    ages[i] = age * age;
+  });
+
+  return Math.trunc(Math.sqrt(ages.reduce((a, b) => a + b, 0)) / 2);
+}
+
+console.log(predictAgeTwo(64, 73, 56, 82, 62, 77, 70, 62));
